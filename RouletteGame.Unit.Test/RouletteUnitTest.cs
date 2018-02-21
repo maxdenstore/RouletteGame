6a69f6e409e29576dc2210c1f1f619e6f74552a4
nicho1991 <ladefoged1991@gmail.com>
2018-02-21 19:58:42 +0100

    fefe

--- RouletteGame.Unit.Test/RouletteUnitTest.cs
+++ RouletteGame.Unit.Test/RouletteUnitTest.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace RouletteGame.Unit.Test
 {
-    [TestFixture]
-    public class Class1
+    class RouletteUnitTest
     {
     }
 }
