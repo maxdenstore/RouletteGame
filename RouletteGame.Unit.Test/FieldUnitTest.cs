238885c876826d0e5d148e2e2d6574546a3ad885
nicho1991 <ladefoged1991@gmail.com>
2018-02-22 11:58:45 +0100

    FieldTestDone

--- RouletteGame.Unit.Test/FieldUnitTest.cs
+++ RouletteGame.Unit.Test/FieldUnitTest.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using RouletteGame.Legacy;
 
 namespace RouletteGame.Unit.Test
 {
+    [TestFixture]
     class FieldUnitTest
     {
+        [Test]
+        public void FieldColor()
+        {
+            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
+            Assert.That(uut.GetResult().Color, Is.EqualTo(2));
+        }
+        [Test]
+        public void FieldEven()
+        {
+            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
+            Assert.That(uut.GetResult().Even, Is.EqualTo(true));
+        }
+        [Test]
+        public void FieldNumer()
+        {
+            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
+            Assert.That(uut.GetResult().Number, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ToStringGreen()
+        {
+            var uut = new Field(0, 2);
+            Assert.That(uut.ToString(), Is.EqualTo("[0, green]"));
+        }
     }
 }
--- RouletteGame.Unit.Test/RouletteUnitTest.cs
+++ RouletteGame.Unit.Test/RouletteUnitTest.cs
@@ -11,26 +11,6 @@ namespace RouletteGame.Unit.Test
     [TestFixture]
     public class RouletteUnitTest
     {
-        [Test]
-        public void FieldColor()
-        {
-            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
-            Field expected = new Field(0,2);
-            Assert.That(uut.GetResult().Color, Is.EqualTo(expected.Color));
-        }
-        [Test]
-        public void FieldEven()
-        {
-            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
-            Field expected = new Field(0, 2);
-            Assert.That(uut.GetResult().Even, Is.EqualTo(expected.Even));
-        }
-        [Test]
-        public void FieldNumer()
-        {
-            var uut = new Roulette(new FakeRandomizerReturn0(), new FakeFieldFactoryGRB());
-            Field expected = new Field(0, 2);
-            Assert.That(uut.GetResult().Number, Is.EqualTo(expected.Number));
-        }
+        
     }
 }
